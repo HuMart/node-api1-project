f9b958a28bd302e30ade5aaa7aecf266b518b351
HuMart <manya01@protonmail.com>
2020-02-04 15:14:39 -0500

    getting the list of elements from db

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require('express');
+
+const Elements = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', function(request, response) {
+    response.send({hello: "server runing"});
+});
+
+server.get('/api/elements', (req, res) => {
+    Elements.find()
+        .then(elements => {
+            console.log("Elements:", elements);
+            res.status(200).json(elements);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({errorMessage: "sorry, we run into an error to get the list of elements"})
+        })
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`\n *** api on port: ${port} *** `))
